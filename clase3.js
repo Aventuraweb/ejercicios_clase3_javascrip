8f8115049bc2087f61b2d885c23714c43b7310f5
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-08-02 19:48:50 -0500

    Implementa métodos públicos establecerNombre(), obtenerNombre(), establecerEdad(), obtenerEdad(), establecerFechaNacimiento() y obtenerFechaNacimiento() para manipular estas propiedades de manera segura por Jaqueline

--- clase3.js
+++ clase3.js
@@ -10,40 +10,27 @@ class Persona {
     }
 
     establecerNombre(nombre) {
-        if ( nombre === 'string' && nombre.trim().length > 0) {
-            this.#nombre = nombre;
-        } else {
-            throw new Error('Nombre inválido');
-        }
+
     }
 
     obtenerNombre() {
-        return this.#nombre;
+
     }
 
     establecerEdad(edad) {
-        if ( edad === 'number' && edad >= 0) {
-            this.#edad = edad;
-        } else {
-            throw new Error('Edad inválida');
-        }
+
     }
 
     obtenerEdad() {
-        return this.#edad;
+
     }
 
     
     establecerFechaNacimiento(fechaNacimiento) {
-        const fecha = new Date(fechaNacimiento);
-        if ( !isNaN(fecha) ) {
-            this.#fechaNacimiento = fecha;
-        } else {
-            throw new Error('Fecha de nacimiento inválida');
-        }
+        
     }
 
     obtenerFechaNacimiento() {
-        return this.#fechaNacimiento;
+
     }
 }
