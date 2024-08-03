60277e3fbc842e0836c5f099550918a4449886d8
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-08-02 20:46:12 -0500

    Implementa un método adicional calcularFechaNacimiento() que calcule la fecha de nacimiento a partir de la edad. por Jaqueline

--- clase3.js
+++ clase3.js
@@ -10,27 +10,53 @@ class Persona {
     }
 
     establecerNombre(nombre) {
+        if ( nombre === 'string' && nombre.trim().length > 0) {
+            this.#nombre = nombre;
+        } else {
+            throw new Error('Nombre inválido');
+        }
 
     }
 
     obtenerNombre() {
-
+        return this.#nombre;
     }
 
     establecerEdad(edad) {
+        if ( edad === 'number' && edad >= 0) {
+            this.#edad = edad;
+        } else {
+            throw new Error('Edad inválida');
+        }
 
     }
 
     obtenerEdad() {
-
+        return this.#edad;
     }
 
     
     establecerFechaNacimiento(fechaNacimiento) {
-        
+        const fecha = new Date(fechaNacimiento);
+        if ( !isNaN(fecha) ) {
+            this.#fechaNacimiento = fecha;
+        } else {
+            throw new Error('Fecha de nacimiento inválida');
+        }
+
     }
 
     obtenerFechaNacimiento() {
-
+        return this.#fechaNacimiento
     }
+
+
+    calcularFechaNacimiento(){
+        const dia = new Date(); 
+        const añoNacimiento = dia.getFullYear() - this.#edad;  
+        const fechaNacimiento = new Date(añoNacimiento, dia.getMonth(), dia.getDate()); 
+        this.#fechaNacimiento = fechaNacimiento; 
+        return this.#fechaNacimiento; 
+    }
+
 }
