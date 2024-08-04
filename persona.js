15048a86a8b939c6b1f20eda0b1991bc029c3975
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-08-03 22:52:29 -0500

    Implementa métodos públicos establecerNombre(), obtenerNombre(), establecerEdad(), obtenerEdad(), establecerFechaNacimiento() y obtenerFechaNacimiento() para manipular estas propiedades de manera segura. por Jaqueline

--- persona.js
+++ persona.js
@@ -1,3 +1,7 @@
+// Encapsulamiento:
+
+// Define una clase Persona con propiedades privadas para el nombre, la edad y la fecha de nacimiento.
+
 class Persona {
     #nombre; 
     #edad; 
@@ -7,6 +11,33 @@ class Persona {
         this.#nombre = nombre; 
         this.#edad = edad;  
         this.#fechaNacimiento = fechaNacimiento;  
+    }
+
+    // Implementa métodos públicos establecerNombre(), obtenerNombre(), establecerEdad(), obtenerEdad(), establecerFechaNacimiento() y obtenerFechaNacimiento() para manipular estas propiedades de manera segura.
+
+    establecerNombre(nombre) {
+    
+    }
+
+    obtenerNombre() {
+        
+    }
+
+    establecerEdad(edad) {
+
+    }
+
+    obtenerEdad() {
+        
+    }
+
+    
+    establecerFechaNacimiento(fechaNacimiento) {
+
+    }
+
+    obtenerFechaNacimiento() {
+       
     }
 
 }
