1fd28dc945a7d5de1fa1ec3be1cbfbc1666aa9d4
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-08-04 01:58:57 -0500

    Implementa el método calcularSalario() en cada subclase de la siguiente manera: ▪ En Gerente, calcula el salario base más un bono. ▪ En Vendedor, calcula el salario base más una comisión sobre las ventas. ▪ En otras subclases, define reglas específicas para calcular el salario.

--- empleado.js
+++ empleado.js
@@ -31,15 +31,53 @@ class Empleado {
 
 class Gerente extends Empleado {
 
+    // Implementa el método calcularSalario() en cada subclase de la siguiente manera: En Gerente, calcula el salario base más un bono.
+    Bono; 
+
+    constructor(nombre, salarioBase, Bono){
+        super(nombre, salarioBase); 
+        this.Bono = Bono; 
+    }
+
+    calcularSalario(){
+        return this.salarioBase + this.Bono; 
+
+    }
 
  }
 
  class Vendedor extends Empleado {
 
+    // Implementa el método calcularSalario() en cada subclase de la siguiente manera: En Vendedor, calcula el salario base más una comisión sobre las ventas
+    comision; 
+    ventas; 
+
+    constructor(nombre, salarioBase, comision, ventas){
+        super(nombre, salarioBase); 
+        this.comision = comision; 
+        this.ventas = ventas; 
+    }
+
+    calcularSalario(){
+        return this.salarioBase + this.comision + this.ventas; 
+    }
 
  }
 
  class JefeVentas extends Empleado {
 
-    
+    // Implementa el método calcularSalario() en cada subclase de la siguiente manera: En otras subclases, define reglas específicas para calcular el salario.
+    comision; 
+    BonificacionPorAntiguedad;
+
+    constructor(nombre, salarioBase, comision, BonificacionPorAntiguedad){
+        super(nombre, salarioBase); 
+        this.comision = comision; 
+        this.BonificacionPorAntiguedad = BonificacionPorAntiguedad; 
+    }
+
+    calcularSalario(){
+        return this.salarioBase + this.comision + this.BonificacionPorAntiguedad; 
+    }
+
  }
