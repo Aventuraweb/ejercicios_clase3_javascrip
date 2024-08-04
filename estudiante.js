3a5f8203bbf6d8c5e430bd3d7fa998979a3ab67c
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-08-03 23:02:59 -0500

    Agrega una propiedad adicional para el grado del estudiante por Jaqueline

--- estudiante.js
+++ estudiante.js
@@ -3,6 +3,20 @@ const Persona = require("./persona");
 //Crea una clase Estudiante que herede de la clase Persona
 class Estudiante extends Persona {
    
+    // Agrega una propiedad adicional para el grado del estudiante
+    #gradoEstudiante; 
+
+    establecergradoEstudiante(gradoEstudiante){
+        if ( gradoEstudiante == 'String' && gradoEstudiante.trim().length > 0){
+            this.#gradoEstudiante = gradoEstudiante; 
+        }else{
+            throw new Error('Grado del estudiante inválida');
+        }
+    }
+    
+   obtenergradoEstudiante(){
+        return this.#gradoEstudiante; 
+    }
 
 }
 
