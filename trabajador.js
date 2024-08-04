7f4b0a4949679c1948a2b28a419ad448dae4d755
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-08-03 23:14:41 -0500

    Define una interfaz Trabajador con un método realizarTarea(). por Jaqueline

---
+++ trabajador.js
@@ -0,0 +1,9 @@
+//Polimorfismo 
+
+//Define una interfaz Trabajador con un método realizarTarea().
+class Trabajador {
+    realizarTarea() {
+        throw new Error('Estudiando desarrollo de software');
+    }
+}
+
