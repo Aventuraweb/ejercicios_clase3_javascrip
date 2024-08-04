a03915397d8d0170123800f4ee47ea5afc34ea95
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-08-04 01:42:50 -0500

    Implementa métodos públicos establecerNombre(), obtenerNombre(), establecerColor() y obtenerColor() para manipular estas propiedades demanera segura.

--- figuraGeometrica.js
+++ figuraGeometrica.js
@@ -1,6 +1,6 @@
 // Encapsulamiento y Herencia:
 
-//Crea una clase base FiguraGeometrica con propiedades privadas para el nombre y el color de la figura.
+// Crea una clase base FiguraGeometrica con propiedades privadas para el nombre y el color de la figura.
 
 class FiguraGeometrica {
 
@@ -10,5 +10,34 @@ class FiguraGeometrica {
     constructor(nombre, color){
         this.#nombre = nombre; 
         this.#color = color; 
+    }
+
+    // Implementa métodos públicos establecerNombre(), obtenerNombre(), establecerColor() y obtenerColor() para manipular estas propiedades demanera segura.
+
+    establecerNombre(nombre){
+        if ( nombre === 'string' && nombre.trim().length > 0) {
+            this.#nombre = nombre;
+        } else {
+            throw new Error('Nombre inválido');
+        }
+
+    }
+
+    obtenerNombre(){
+        return this.#nombre
+
+    }
+
+    establecerColor(color){
+        if ( color === 'string' && nombre.trim().length > 0) {
+            this.#color = color;
+        } else {
+            throw new Error('color inválido');
+        }
+
+    }
+
+    obtenerColor(){
+        return this.#color
     }
 }
