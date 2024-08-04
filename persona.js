c57ce8e3f86faa8d2f4b29e90a56f929a63d7294
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-08-03 22:54:59 -0500

    Agrega validaciones en los métodos de establecimiento para garantizar que los valores proporcionados sean válidos (por ejemplo, la edad no puede ser negativa). por Jaqueline

--- persona.js
+++ persona.js
@@ -16,28 +16,45 @@ class Persona {
     // Implementa métodos públicos establecerNombre(), obtenerNombre(), establecerEdad(), obtenerEdad(), establecerFechaNacimiento() y obtenerFechaNacimiento() para manipular estas propiedades de manera segura.
 
     establecerNombre(nombre) {
-    
+        // Agrega validaciones en los métodos de establecimiento para garantizar que los valores proporcionados sean válidos (por ejemplo, la edad no puede ser negativa).
+        if ( nombre === 'string' && nombre.trim().length > 0) {
+            this.#nombre = nombre;
+        } else {
+            throw new Error('Nombre inválido');
+        }
+
     }
 
     obtenerNombre() {
-        
+        return this.#nombre;
     }
 
     establecerEdad(edad) {
+        if ( edad === 'number' && edad >= 0) {
+            this.#edad = edad;
+        } else {
+            throw new Error('Edad inválida');
+        }
 
     }
 
     obtenerEdad() {
-        
+        return this.#edad;
     }
 
     
     establecerFechaNacimiento(fechaNacimiento) {
+        const fecha = new Date(fechaNacimiento);
+        if ( !isNaN(fecha) ) {
+            this.#fechaNacimiento = fecha;
+        } else {
+            throw new Error('Fecha de nacimiento inválida');
+        }
 
     }
 
     obtenerFechaNacimiento() {
-       
+        return this.#fechaNacimiento
     }
 
 }
